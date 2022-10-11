c004a789e1a1abdbee3d71979fd1d4c433388e74
mskdmage <49457732+mskdmage@users.noreply.github.com>
2022-10-11 01:04:54 -0500

    Structure done

---
+++ 01_creador_de_nombres/app.py
@@ -0,0 +1,11 @@
+print("<-- RESPONDE ESTAS PREGUNTAS POR FAVOR -->")
+
+ciudad = input("Que ciudad te gustaria visitar?: ")
+color = input("Cual es tu color favorito?: ")
+
+print("El nombre de tu cerveza\nes '" + ciudad + " " + color)
+print("\nFelicitaciones!")
+
+
+
+
---
+++ 02_calculador_de_comisiones/app.py
@@ -0,0 +1,7 @@
+print("<-- RESPONDE ESTAS PREGUNTAS POR FAVOR -->")
+
+nombre = input("Por favor, dime tu nombre: ")
+ventas = int(input("Diga sus ventas totales del mes: "))
+comision = round(ventas * 13 / 100,2)
+
+print(f"Hola {nombre}, tus comisiones de este mes son de ${comision}")
